75c4aa27187a93bfc171ebca698faa56b3f73066
Kacper Gajda <lorenstenlelak@gmail.com>
2023-11-20 14:52:10 +0100

    working single file query

--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -1,18 +1,22 @@
 const projects = [
   {
     title: 'C2 Montreal',
+    src: 'c2montreal',
     color: '#000000',
   },
   {
     title: 'Office Studio',
+    src: 'officeStudio',
     color: '#8c8c8c',
   },
   {
     title: 'Locomotive',
+    src: 'locomotive',
     color: '#efe803',
   },
   {
     title: 'Silencio',
+    src: 'silencio',
     color: '#706d63',
   },
 ];
--- src/components/projects/modal/index.js
+++ src/components/projects/modal/index.js
@@ -8,13 +8,24 @@ import * as styles from './style.module.scss';
 function Modal({ modal }) {
   const data = useStaticQuery(graphql`
     query {
-      allFile(filter: { relativePath: { regex: "modal/" } }) {
-        edges {
-          node {
-            childImageSharp {
-              gatsbyImageData(placeholder: BLURRED, formats: WEBP)
-            }
-          }
+      c2montreal: file(relativePath: { regex: "modal/c2montreal.png/" }) {
+        childImageSharp {
+          gatsbyImageData(placeholder: BLURRED, formats: WEBP)
+        }
+      }
+      officeStudio: file(relativePath: { regex: "modal/officestudio.png/" }) {
+        childImageSharp {
+          gatsbyImageData(placeholder: BLURRED, formats: WEBP)
+        }
+      }
+      locomotive: file(relativePath: { regex: "modal/locomotive.png/" }) {
+        childImageSharp {
+          gatsbyImageData(placeholder: BLURRED, formats: WEBP)
+        }
+      }
+      silencio: file(relativePath: { regex: "modal/silencio.png/" }) {
+        childImageSharp {
+          gatsbyImageData(placeholder: BLURRED, formats: WEBP)
         }
       }
     }
@@ -23,8 +34,8 @@ function Modal({ modal }) {
   return (
     <div className={styles.wrapper}>
       <div className={styles.modalSlider}>
-        {projects.map(({ title, color }, index) => {
-          const image = getImage(data.allFile.edges[index].node.childImageSharp.gatsbyImageData);
+        {projects.map(({ title, src, color }, index) => {
+          const image = getImage(data[src]);
 
           return (
             <div className={styles.modal} key={index} style={{ backgroundColor: color }}>
